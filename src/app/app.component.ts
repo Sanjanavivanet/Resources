fed0e743d71f11f8aec85c242bb911375203070c
unknown <sanjanapatil94824@gmail.com>
2024-02-01 23:06:43 +0530

    first changes

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+// src/app/app.component.ts
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ResponseService } from './response-form.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  constructor(private responseService: ResponseService) {}
+
+  submitForm(formData: any): void {
+    this.responseService.addResponse(formData);
+
+    // Download responses as JSON file
+    this.responseService.downloadResponsesAsJson();
+  }
+}
